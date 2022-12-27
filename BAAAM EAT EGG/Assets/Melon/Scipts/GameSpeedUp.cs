84fce652761da4afe10a5fb176cf46c96c2dcd12
Joonieboy <zkzkshsh@gmail.com>
2022-12-27 17:14:48 +0900

    pattern

--- BAAAM EAT EGG/Assets/Melon/Scipts/GameSpeedUp.cs
+++ BAAAM EAT EGG/Assets/Melon/Scipts/GameSpeedUp.cs
@@ -7,7 +7,7 @@ public class GameSpeedUp : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0))
         {
             StartCoroutine(speedUp());
         }
